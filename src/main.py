283fb388f7eb699099187ec8e969f9bf3c1a963e
yonzro1610 <yonzro1610@proton.me>
2024-05-22 18:21:29 -0600

    start ui dev

--- src/main.py
+++ src/main.py
@@ -9,9 +9,11 @@ import keyboard
 import pygame
 import os
 
+# Clear console
 os.system("@echo off")
 os.system("cls")
 
+# Check for Controllers
 print(f"{Fore.CYAN}Checking for PS4 Controllers...")
 controller = checkforcontroller.main()
 if controller == False:
@@ -20,3 +22,10 @@ else:
     print("PS4 Controller Detected!")
 
 print(f"{Fore.WHITE}")
+
+
+
+# Minimize
+import win32gui
+win = win32gui.GetForegroundWindow()
+win32gui.ShowWindow(win, 6)

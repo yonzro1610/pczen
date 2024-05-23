f64d6a6fc3afa1345e50e17c77e19527f5e4395c
yonzro1610 <yonzro1610@proton.me>
2024-05-22 21:44:22 -0600

    revamp

---
+++ note.md
@@ -0,0 +1,3 @@
+- This project is in alpha.
+Expect bugs & broken features.
+I will be making a discord server soon, you will be able to join and report bugs.
--- src/scripts/modules/interface/maketab.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from tkinter import ttk
-
-def tab(name, notebook):
-    tab = ttk.Frame(notebook)
-    notebook.add(tab, text=name)
-
-    return tab
